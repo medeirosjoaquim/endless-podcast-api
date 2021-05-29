4426aaa86a0e051b2ffc49514338563cc1b6ab8c
JohnBox <johnboxcodes@gmail.com>
2021-05-28 22:07:04 -0300

    handle errors for URL

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -10,61 +10,13 @@ class PodcastsController < ApplicationController
   end
   
   def fetch_rss
-    url = session[:podcast_rss_url]
+    #url = session[:podcast_rss_url]
     data = JSON.parse(request.body.read)
     rss_results = []
-    puts(data['url'])
+    url = data['url']
+    url.prepend "https://" unless url.start_with?('http://', 'https://')
 
-    podcast = Podcast.where(url: data['url']).take
-
-    if podcast
-      render json: podcast
-    else
-      render plain: 'no data'
-    end
-
-    begin
-      #url.prepend "http://" unless url.start_with?('http://', 'https://')
-
-      @rss = RSS::Parser
-          .parse(URI.open(data['url'])
-          .read, false)
-    rescue => e
-      puts(e)
-      session[:podcast_rss_url] = nil
-    else 
-      @rss.each do |result|
-        result = { title: result.title,
-          date: result.pubDate,
-          link: result.link,
-          description: result.description,
-          audio_url: result.enclosure.url }
-        rss_results.push(result)
-      end
-      response = rss_results
-      puts (@rss)
-    end
-    render json: {data: rss_results}
-  end
-end
-class PodcastsController < ApplicationController
-
-  require "rss"
-  require "open-uri"
-
-  skip_before_action :verify_authenticity_token, :only => [:index, :fetch_rss]
-  
-  def index
-    render plain: 'Hello, World!'
-  end
-  
-  def fetch_rss
-    url = session[:podcast_rss_url]
-    data = JSON.parse(request.body.read)
-    rss_results = []
-    puts(data['url'])
-
-    podcast = Podcast.where(url: data['url']).take
+    podcast = Podcast.where(url: url).take
 
     if podcast
       render json: {
@@ -75,15 +27,15 @@ class PodcastsController < ApplicationController
 				feed: podcast.feed
 			}
     else
-      #render plain: 'no data'
 			begin
 				rss = RSS::Parser
-						.parse(URI.open(data['url'])
+						.parse(URI.open(url)
 						.read, false)
+        
 				feed = rss.items[0..5]
 			rescue => e
 				puts(e)
-				session[:podcast_rss_url] = nil
+				render plain: 'Not Found', :status => '404'
 			else 
 				feed.each do |result|
 					result = { title: result.title,
@@ -92,29 +44,27 @@ class PodcastsController < ApplicationController
 						description: result.description,
 						audio_url: result.enclosure.url }
 					rss_results.push(result)
+					title = rss.channel.title
+					summary = rss.channel.itunes_summary
+					keywords = rss.channel.itunes_keywords
+					category = rss.channel.itunes_category.text
+					Podcast.new(title: title, 
+					summary: summary,
+					url: data['url'], 
+					category: category, 
+					keywords: keywords,
+					feed: rss_results
+					).save
+					
+					render json: {
+						title: title,
+						summary: summary,
+						keywords: keywords,
+						category: category,
+						feed: rss_results
+					}
 				end
-				#response = rss_results
 			end
-			title = rss.channel.title
-			summary = rss.channel.itunes_summary
-			keywords = rss.channel.itunes_keywords
-			category = rss.channel.itunes_category.text
-			Podcast.new(title: title, 
-			summary: summary,
-			url: data['url'], 
-			category: category, 
-			keywords: keywords,
-			feed: rss_results
-			).save
-			render json: {
-				title: title,
-        summary: summary,
-        keywords: keywords,
-        category: category,
-				feed: rss_results
-			}
     end
-
-    
   end
 end
